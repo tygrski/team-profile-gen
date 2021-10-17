2fd961f2e798d36819a3355b7bcf9ec1d5e0f7e7
tygrski <77369211+tygrski@users.noreply.github.com>
2021-10-17 14:36:37 -0500

    question bugs

--- index.js
+++ index.js
@@ -107,7 +107,43 @@ function internQuestions() {
     });
   };
 
+  function writeToFile(fileName, data) {
+    fs.writeFile('./lib/team.html', data, err => {
+      if(err) {
+        console.log(err)
+      }
+      return "team profile generated !";
+    });
+  };
+  
+ // generate page
+ function genratePage(data) {
+ 
+  `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<div class="card" style="width: 18rem;">
 
-// generate page
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+   <p class="card-text"></p>
+   <p class="card-text"></P>
+</div>
+</div>
+
+
+
+</body>
+</html>
+  `
+  
+};
 
 startQuestions();
